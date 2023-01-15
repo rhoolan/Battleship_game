a7d6f4b5ea985022aa5702148bf573c7ec4794a8
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 21:22:53 +0900

    add [] function

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,15 @@
 class Board
-  
+    attr_reader :size
+
     def initialize(boardsize)
         @grid = Array.new(boardsize) {Array.new(boardsize, :N)}
         @size = boardsize * boardsize
+    end
+
+    def [](position)
+        row = position[0]
+        column = position[1]
+
+        @grid[row][column]
     end
 end
