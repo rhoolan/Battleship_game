9d4b7bb678d1841a557b43c1e0f0763464eeb403
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 22:48:41 +0900

    add get_move function

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,9 @@
 class Player
 
+    def get_move
+        puts 'enter a position with coordinates separated with a space like 4 7: '
+        position = gets.chomp
+
+        [position[0].to_i, position[-1].to_i]
+    end
 end
