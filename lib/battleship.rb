e3ba21d4843186658b2dfafff89cf030ffbf27df
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 22:58:38 +0900

    add start_game function

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -3,10 +3,16 @@ require_relative "player"
 
 class Battleship
     attr_reader :board, :player
-    
+
     def initialize(length)
         @player = Player.new
         @board = Board.new(length)
         @remaining_misses = (length*length) / 2
+    end
+
+    def start_game 
+        board.place_random_ships
+        print board.num_ships
+        board.print
     end
 end
