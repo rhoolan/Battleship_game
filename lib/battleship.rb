08b3f31adcf1356f0e4d17d5d446b9dbcfe5cb09
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 22:56:50 +0900

    add @player @board attr_reader

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -2,7 +2,8 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
-
+    attr_reader :board, :player
+    
     def initialize(length)
         @player = Player.new
         @board = Board.new(length)
