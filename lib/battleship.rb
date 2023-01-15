4e6a4521a6bbfcb6de52c2d3c63ea9f648995806
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 22:54:57 +0900

    add initialize function

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -3,4 +3,9 @@ require_relative "player"
 
 class Battleship
 
+    def initialize(length)
+        @player = Player.new
+        @board = Board.new(length)
+        @remaining_misses = (length*length) / 2
+    end
 end
